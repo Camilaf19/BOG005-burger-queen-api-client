3ae7edd294b31f9c2ffb15ca721e574d2fc31de9
Camilaf19 <cam.fonseca19@gmail.com>
2022-11-24 10:24:23 -0500

    Ajustes a la maquetacion del rol de Admin

--- src/components/AdminProfile.jsx
+++ src/components/AdminProfile.jsx
@@ -19,24 +19,7 @@ export function AdminProfile() {
                     </ul>
                 </nav>
                 <article className='feedAdmin'>
-                    <ul className='listProductsAdmin'>
-                        <li className='productAdmin'><img className='imageProductAdmin' 
-                        src={require('../img/coffee.png')} alt='Product Image' />American Coffee</li>
-                        <li className='productAdmin'><img className='imageProductAdmin' 
-                        src={require('../img/coffewithmilk.png')} alt='Product Image' />Coffee With Milk</li>
-                        <li className='productAdmin'><img className='imageProductAdmin' 
-                        src={require('../img/sandwich.png')} alt='Product Image'/>Ham and Cheese Sandwich</li>
-                        <li className='productAdmin'><img className='imageProductAdmin' 
-                        src={require('../img/juice.png')} alt='Product Image' />Natural Fruit Juice</li>
-                        <li className='productAdmin'>Simple Hamburger</li>
-                        <li className='productAdmin'>Double hamburger</li>
-                        <li className='productAdmin'>French Fries</li>
-                        <li className='productAdmin'>Onion Rings</li>
-                        <li className='productAdmin'>Water 500ml</li>
-                        <li className='productAdmin'>Water 750ml</li>
-                        <li className='productAdmin'>Drink/Soda 500ml</li>
-                        <li className='productAdmin'>Drink/Soda 750ml</li>
-                    </ul>
+                    
                     <button className='buttonNewProduct'>Add new product + </button>
                 </article>
                 <button className='buttonSaveAll'>Save</button>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-//import { LoginView } from './Login'
-import { AdminProfile } from './components/AdminProfile';
+import { LoginView } from './Login'
+//import { AdminProfile } from './components/AdminProfile';
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <AdminProfile />
+    <LoginView/>
 );
 
 
